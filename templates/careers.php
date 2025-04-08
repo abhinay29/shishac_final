e2a40418b983ca18f7082dcb25c5b249ceada4b8
Abhinay Sharma <abhinaysharma.vw@gmail.com>
2025-04-08 17:41:58 +0530

    Update career page

--- templates/careers.php
+++ templates/careers.php
@@ -10,7 +10,9 @@ include 'common/header.php';
 
 <div class="section-padding-60 bg-light">
   <div class="container">
-    <h2 class="fw-bold mb-4 section-title">Join Our Team</h2>
+    <div class="mb-4">
+      <h2 class="fw-bold section-title">Join Our Team</h2>
+    </div>
 
     <p class="mb-5">At Shish Advance Composites, we are driven by innovation and excellence in the world of advanced composite materials. We believe that our people are our greatest asset, and we are always looking for passionate and talented individuals to join our team. We offer a dynamic and challenging work environment where you can grow and develop your career.</p>
 
@@ -18,8 +20,12 @@ include 'common/header.php';
     <div class="row">
       <div class="col-lg-5 pe-lg-5">
         <h4 class="mb-4">Apply Now</h4>
-        <form action="YOUR_FORM_HANDLER_URL" method="POST" enctype="multipart/form-data">
-
+        <form action="https://api.web3forms.com/submit" method="POST" enctype="multipart/form-data" id="career_form">
+          <input type="hidden" name="access_key" value="07435991-8eab-4a8f-8ef1-9cd4e7ea9e5a">
+          <input type="hidden" name="subject" value="Career form Submission from SHISH Advanced Composites Website">
+          <input type="hidden" name="from_name" value="Webmaster - SHISH Advanced Composites">
+          <input type="checkbox" name="botcheck" id="" style="display: none;">
+          <input type="hidden" name="redirect" value="https://web3forms.com/success">
           <div class="mb-3">
             <label for="name" class="form-label">Name:</label>
             <input type="text" id="name" name="name" class="form-control" required>
@@ -32,7 +38,7 @@ include 'common/header.php';
 
           <div class="mb-3">
             <label for="formFile" class="form-label">Upload Resume:</label>
-            <input class="form-control" type="file" id="resume" name="resume" accept=".pdf, .doc, .docx" required>
+            <input class="form-control" type="file" id="resume" name="attachment" accept=".pdf, .doc, .docx" required>
           </div>
 
           <div class="mb-3">
@@ -57,3 +63,10 @@ include 'common/header.php';
 include 'common/footer.php';
 
 ?>
+
+<script>
+  window.onload = function() {
+    // Reset the form fields when the page loads
+    document.getElementById("career_form").reset();
+  };
+</script>
